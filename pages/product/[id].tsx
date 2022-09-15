9e9ec96067eb628b708f799c4160ee91b89fbe33
David Leal Peláez <davidlealpelaez@gmail.com>
2022-09-15 08:37:44 -0500

    Conexión a la BD de aguacates

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 const ProductPage = () => {
-  const { query } = useRouter()
+  const [product, setProduct] = useState<TProduct>()
+  const {
+    query: { id },
+  } = useRouter()
 
+  useEffect(() => {
+    if (id) {
+      window
+        .fetch(`/api/avo/${id}`)
+        .then((response) => response.json())
+        .then((data) => setProduct(data))
+    }
+  }, [id])
+
+  console.log(useRouter())
   return (
     <section>
-      <h1>Página producto: {query.id}</h1>
+      <h1>Página producto: {product?.name}</h1>
     </section>
   )
 }
